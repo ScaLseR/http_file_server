00b1eb6bb64ee22c5872aa2e0f98020c6fcd6b88
a.skorik <scalserstv@yandex.ru>
2022-04-13 17:54:13 +0300

    added _set_headers edited do_POST, do_DELETE, do_GET, load_from_db

--- server.py
+++ server.py
@@ -2,48 +2,59 @@
 import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib.parse import parse_qs, urlparse
+from sql_db import SqlStorage
 
 
 class ApiEndpoint(BaseHTTPRequestHandler):
     """класс дял получения и обработки запросов на эндпоинты"""
 
+    def _set_headers(self, id_response: int):
+        """формирование хедера с указанным статусом ответа"""
+        self.send_response(id_response)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
     def do_GET(self):
+        """отработка запросов GET на ендпоинты /api/get и /api/download"""
         if self.path.startswith('/api/get'):
             params = parse_qs(urlparse(self.path).query)
+            if len(params) == 0:
+                rez = storage.load_from_db()
             print(params)
-            print('/api/get')
         if self.path.startswith('/api/download'):
             params = parse_qs(urlparse(self.path).query)
             print(params)
             print('/api/download')
-        self.send_response(200)
-        self.send_header('Content-Type', 'application/json')
-        self.end_headers()
+        self._set_headers(200)
         self.wfile.write(str(params).replace("'", '"').encode('utf-8'))
 
     def do_POST(self):
-        params = parse_qs(urlparse(self.path).query)
-        print(params)
-        print('/api/upload')
+        """отработка запросов POST на ендпоинт /api/upload"""
         if self.path.startswith('/api/upload'):
-            self.send_response(201)
-            self.send_header('Content-Type', 'application/json')
-            self.end_headers()
+            self._set_headers(201)
+            params = parse_qs(urlparse(self.path).query)
+            print(params)
+            # content_len = int(self.headers('content-length'))
+            # post_body = self.rfile.read(content_len)
+            # print(post_body)
             self.wfile.write('POST_forever'.encode('utf-8'))
 
     def do_DELETE(self):
+        """отработка ёзапроса DELETE на ендпоинт /api/delete"""
         if self.path.startswith('/api/delete'):
             params = parse_qs(urlparse(self.path).query)
             if len(params) == 0:
-                self.send_response(400)
-                self.send_header('Content-Type', 'application/json')
-                self.end_headers()
-                self.wfile.write('No conditions'.encode('utf-8'))
+                self._set_headers(400)
+                self.wfile.write('отсутствуют условия'.encode('utf-8'))
             else:
-                self.send_response(200)
-                self.send_header('Content-Type', 'application/json')
-                self.end_headers()
+                self._set_headers(200)
                 self.wfile.write('X files deleted'.encode('utf-8'))
 
-server = HTTPServer(("127.0.0.1", 9876), ApiEndpoint)
-server.serve_forever()
+
+def run(ip_addr: str, port: int):
+    server = HTTPServer((ip_addr, port), ApiEndpoint)
+    server.serve_forever()
+
+if __name__ == "__main__":
+    run('127.0.0.1', 9876)
+    storage = SqlStorage('file_storage')
--- sql_db.py
+++ sql_db.py
@@ -19,21 +19,24 @@ class SqlStorage:
         self._connection.commit()
 
     def save_to_db(self, data: dict):
-        """сохраняем в базу значения data"""
+        """сохраняем в базу параметры файла"""
         cursor = self._connection.cursor()
         for key, value in data.items():
             cursor.execute('insert or ignore into historydata(date, price) values (?, ?)',
                            (key, value))
         self._connection.commit()
 
-    def load_from_db(self, start: datetime, end: datetime) -> dict:
-        """получаем из базы значения в интревале start -> end"""
-        cursor = self._connection.cursor()
-        cursor.execute("select * from historydata where date >= " + "'" + str(start) +
-                       "'" + " and date < " + "'" + str(end) + "'")
-        result = cursor.fetchall()
-        self._connection.commit()
-        data_dict = {}
-        for data in result:
-            data_dict[data[0]] = data[1]
+    def load_from_db(self, **kwargs) -> dict:
+        """получаем из базы файлы соответствующие условиям"""
+        if len(kwargs) == 0:
+            cursor = self._connection.cursor()
+            cursor.execute("select * from fileserver")
+            result = cursor.fetchall()
+            print(result)
+            self._connection.commit()
+            data_dict = {}
+        #   for data in result:
+        #         data_dict[data[0]] = data[1]
+        else:
+            pass
         return data_dict
