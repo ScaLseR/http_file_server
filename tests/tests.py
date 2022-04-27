cceea0a939b76857efa5002691d1cb352683d695
a.skorik <scalserstv@yandex.ru>
2022-04-27 10:26:44 +0300

    added tests.py

---
+++ tests/tests.py
@@ -0,0 +1,16 @@
+from unittest.case import TestCase
+
+
+class EmptyStorageTests(TestCase):
+    """тесты для пустой базы """
+    pass
+
+
+class OneFileStorageTests(TestCase):
+    """тесты для базы содержащей 1 файл"""
+    pass
+
+
+class ManyFilesStorageTests(TestCase):
+    """тесты для базы содержащей большое количество файлов"""
+    pass
